e2547893987d1ee0fc4068bd4466b977ab394de8
highright96 <nsw5296@naver.com>
2022-07-31 20:22:10 +0900

    feat: add GlobalException

---
+++ presentation/src/main/kotlin/com/example/issueservice/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,38 @@
+package com.example.issueservice.exception
+
+import mu.KotlinLogging
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val logger = KotlinLogging.logger {}
+
+    companion object {
+        private const val UNDEFINED_SERVER_ERROR_MESSAGE = "Server Undefined Internal Error"
+    }
+
+    @ExceptionHandler(ServerException::class)
+    fun handleServerException(e: ServerException): ErrorResponse {
+        logger.error { ErrorLogResponse(e.code, e.message, e.cause) }
+        return ErrorResponse(e.code, e.message)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleUndefinedException(e: Exception): ErrorResponse {
+        logger.error { ErrorLogResponse(500, UNDEFINED_SERVER_ERROR_MESSAGE, e.cause) }
+        return ErrorResponse(500, UNDEFINED_SERVER_ERROR_MESSAGE)
+    }
+}
+
+data class ErrorResponse(
+    val code: Int,
+    val message: String
+)
+
+data class ErrorLogResponse(
+    val code: Int,
+    val message: String,
+    val cause: Throwable?
+)
